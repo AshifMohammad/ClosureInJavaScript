de49d9e9ec4f508b3b733eeff0d60d2d6ec85084
Ashif Zafar <ashif.mohammad@cdk.com>
2018-11-27 16:53:01 +0530

    updatedJs

--- ExampleWithInnerFunction.js
+++ ExampleWithInnerFunction.js
@@ -1,25 +1,18 @@
-const addTo = (external) => {
-    const add = (inner) => {
+var addTo = function(external){
+    var add= function(inner){
         return external + inner
     }
     return add;
 }
 
-console.log(addTo(3)) //this will be undefined cuz inner variable is still undefined
+//console.log(addTo(3)) //this will be undefined cuz inner variable is still undefined
 
 //Closure is a property which stores the variable out the outer funciton and returns 
 
-console.dir(addTo(3))
+//console.dir(addTo(3));
+var addThree = new addTo(3.1); //passing inner variable to add function which is a inner function of addTo parent fucniton
+console.dir(addThree);
 
 
-    //Console output :
-
-//     (inner)=> {
-//     return external + inner
-// }
-//  add()
-//  arguments: (...)caller: (...)length: 1name: "add"__proto__: ƒ()[[FunctionLocation]]: 
-//  [Scopes]]:  Scopes[2]
-//  0: Closure(addTo)
-//     external: 3
-//   1: Global { type: "global", name: "", object: Window }
+console.log(addThree(2.9));
+//output : 6
